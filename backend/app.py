27887b1c09662d22ecf047d2e3f7ad13c56c2c5c
clydeopendabichanga'a <clydeopenda@gmail.com>
2026-09-17 23:09:43 +0300

    feat: add transaction API

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+transactions = []
 
 
 @app.route("/")
@@ -10,6 +12,29 @@ def home():
     return jsonify({
         "message": "Smart Expense Tracker API is running"
     })
+
+
+@app.route("/api/transactions", methods=["GET"])
+def get_transactions():
+    return jsonify(transactions)
+
+
+@app.route("/api/transactions", methods=["POST"])
+def add_transaction():
+    data = request.get_json()
+
+    transaction = {
+        "id": len(transactions) + 1,
+        "type": data.get("type"),
+        "description": data.get("description"),
+        "amount": data.get("amount"),
+        "category": data.get("category"),
+        "date": data.get("date"),
+    }
+
+    transactions.append(transaction)
+
+    return jsonify(transaction), 201
 
 
 if __name__ == "__main__":
