c3d5883aec899e920f72cc12e3856d45e291e2c9
clydeopendabichanga'a <clydeopenda@gmail.com>
2026-09-17 22:32:15 +0300

    feat: create Flask backend

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Smart Expense Tracker API is running"
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
